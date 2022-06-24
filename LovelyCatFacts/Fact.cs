c19d2b8ddbac7b605196978adba77bab7d331f03
Prince Anire <prince@hiro-c.net>
2022-06-24 12:24:50 +0800

    Add project files.

---
+++ LovelyCatFacts/Fact.cs
@@ -0,0 +1,22 @@
+﻿namespace LovelyCatFacts
+{
+    public class Fact
+    {
+        public string Id { get; set; }
+        public int VersionNumber { get; set; }
+        public string? User { get; set; }
+        public string Text { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? SendDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public string? Source { get; set; }
+
+        public string? Type { get; set; }
+        public int SentCount { get; set; }
+
+
+
+
+    }
+}
